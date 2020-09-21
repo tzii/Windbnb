534598b8b198d12663496da3d9e6d25a142e3863
Tuấn Vũ <tuanvundvn@gmail.com>
2020-09-21 20:57:31 +0700

    Updates

---
+++ precache-manifest.708a8416051feef5264648242ecbf7d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16cb00bda455084054cb354423b2def",
+    "url": "/Windbnb/index.html"
+  },
+  {
+    "revision": "ae4a9729bbe470dc8727",
+    "url": "/Windbnb/static/css/main.27d41517.chunk.css"
+  },
+  {
+    "revision": "8fa8ae99ff64d24bf13d",
+    "url": "/Windbnb/static/js/2.3c7ba0be.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/Windbnb/static/js/2.3c7ba0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4a9729bbe470dc8727",
+    "url": "/Windbnb/static/js/main.11218629.chunk.js"
+  },
+  {
+    "revision": "ebb69fa506ce1ec895f8",
+    "url": "/Windbnb/static/js/runtime-main.a67e1aa4.js"
+  },
+  {
+    "revision": "54ee1f4ab8ad7c2b788a0a594a55db44",
+    "url": "/Windbnb/static/media/logo.54ee1f4a.svg"
+  }
+]);
